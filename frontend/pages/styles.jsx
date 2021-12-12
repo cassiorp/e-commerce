b1ca9e696f66493c1f6cf38e1b74fc3c2d8b78da
Eduardo Duarte <dudu_duarte@hotmail.com>
2021-12-11 22:58:11 -0300

    feat(login): insert login page

---
+++ frontend/pages/styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const BodyLoginPage = styled.div`
+  height: 100%;
+  background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.65) 50%,
+      transparent 50%
+    ),
+    url('/backImage.png') no-repeat;
+  background-size: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+`;
